9ae1e0651c0e9a4309c4869282c6d1d21171dd99
Stefanie <marystefaniebermudez@gmail.com>
2025-08-26 21:32:36 -0500

    Update STATIC_URL in settings for consistency with deployment environment

--- Hotel/settings.py
+++ Hotel/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
